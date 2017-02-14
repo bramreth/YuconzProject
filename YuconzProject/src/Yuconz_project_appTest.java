a0cf847182f430d0b1007927db6c5df630af84b0
bramreth <bramreth@gmail.com>
2017-02-14 09:44:59 +0000

    TestClass done and working, jdk 1.8 required and junit 5

--- YuconzProject/src/Yuconz_project_appTest.java
+++ YuconzProject/src/Yuconz_project_appTest.java
@@ -1,51 +1,55 @@
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Created by bramreth on 2/13/17.
+ * Created by bramreth on 2/14/17.
  */
-public class Yuconz_project_app_Test {
+class Yuconz_project_appTest {
 
     private Yuconz_project_app app;
 
-    @Before
-    public void setupTest(){
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
         app = new Yuconz_project_app();
     }
-    @After
-    public void tearDownTest(){
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
         app = null;
     }
-    @Test
+
+    @org.junit.jupiter.api.Test
     public void testValidLogin(){
+        System.out.println("Valid login");
         boolean result = app.login("andy","example");
         assertTrue(result);
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void testInValidLogin(){
+        System.out.println("Inalid login");
         boolean result = app.login("notARealName","INVALIDPASSWORD");
         assertFalse(result);
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void noLogin(){
+        System.out.println("no login given");
         boolean result = app.login("","");
         assertFalse(result);
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void testInValidLogout(){
+        System.out.println("inalid logout");
         boolean result = app.logout();
         assertFalse(result);
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void testValidLogout(){
+        System.out.println("Valid logout");
         boolean resultA = app.login("andy","example");
         boolean resultB = app.logout();
         assertTrue(resultB);

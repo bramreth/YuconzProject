f07f88c4aeb613ce571e152787239f5da2c7132d
mtaubert <m.taubert2015@gmail.com>
2017-02-20 14:20:39 +0000

    Authorisation added

--- YuconzProject/src/Authorisation.java
+++ YuconzProject/src/Authorisation.java
@@ -1,5 +1,26 @@
 /**
- * Created by bramreth on 2/20/17.
+ * Created by Maximilian on 20/02/2017.
  */
 public class Authorisation {
+
+    /**
+     * Constructor
+     * Initialises the authorization object
+     */
+    public Authorisation()
+    {
+
+    }
+
+    /**
+     * authorisationCheck
+     * Checks if a given user is authorised to take an action
+     * @param currentUser
+     * @param action
+     * @return true if the user can take the action, otherwise false
+     */
+    public boolean authorisationCheck(User currentUser, String action)
+    {
+        return true;
+    }
 }
